40e2901945e8b8c14ef1c6db61cff052532f87e5
lopezfjose <jflopezfernandez@gmail.com>
2018-11-21 15:40:24 -0500

    Refreshing my memory on passing parameters through subroutines, although this simple program does depend on the four global state variables.

--- src/main.pl
+++ src/main.pl
@@ -1,9 +1,63 @@
 #/usr/bin/perl
+use utf8;
+use v5.26;
+
 use strict;
 use warnings;
 
 
-my $DirectoryName = "src";
+my ($length, $width, $height, $volume) = (0, 0, 0, 0);
 
-#mkdir($DirectoryName) or die;
-#rmdir($DirectoryName) or die;
+sub Volume {
+    my ($length, $width, $height) = @_;
+
+    return ($length * $width * $height);
+}
+
+sub EnterLength {
+    print "Enter length: ";
+    chomp($length = <STDIN>);
+}
+
+sub EnterWidth {
+    print "Enter width: ";
+    chomp($width = <STDIN>);
+}
+
+sub EnterHeight {
+    print "Enter height: ";
+    chomp($height = <STDIN>);
+}
+
+sub PrintLength {
+    say "Length: $length";
+}
+
+sub PrintWidth {
+    say "Width: $width";
+}
+
+sub PrintHeight {
+    say "Height: $height";
+}
+
+sub PrintVolume {
+    say "Volume: $volume";
+}
+
+sub PrintDimensions {
+    my ($l, $w, $h) = @_;
+
+    PrintLength();
+    PrintWidth();
+    PrintHeight();
+    PrintVolume();
+}
+
+EnterLength();
+EnterWidth();
+EnterHeight();
+
+$volume = Volume($length, $width, $height);
+
+PrintDimensions();
